f98466e30fe36e6440c56a820af34ed8acdb7985
Dominik Stadler <dominik.stadler@gmx.at>
2022-08-08 14:29:54 +0200

    Local: Build-script to use proper version of Maven
    Set Java 17
    Run "package validate verify" if no target is specified

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "${OSTYPE}" == "msys" ];then
+  echo "Run this in a cygwin terminal, not MSYS/Mingw"
+  exit 1
+fi
+
+. setJava17.sh
+echo
+
+if [ "${OSTYPE}" == "cygwin" ];then
+  MVN=/cygdrive/c/workspaces/devtools/apache-maven-3.9.6/bin/mvn
+else
+  MVN=/opt/apache/maven/apache-maven-3.9.7/bin/mvn
+fi
+
+if [ $# -eq 0 ];then
+  "${MVN}" package validate verify
+else
+  "${MVN}" "$@"
+fi
