1822834e219a52719fbc3d6a1f236b42c3cf10bb
Dominik Stadler <dominik.stadler@gmx.at>
2025-02-07 20:08:05 +0100

    Use Java 17 and higher version of Maven

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+. setJava17.sh
+
 if [ "${OSTYPE}" == "cygwin" ];then
   MVN=/cygdrive/c/workspaces/devtools/apache-maven-3.8.6/bin/mvn
 else
-  MVN=/opt/apache/maven/apache-maven-3.9.0/bin/mvn
+  MVN=/opt/apache/maven/apache-maven-3.9.7/bin/mvn
 fi
 
 if [ $# -eq 0 ];then
